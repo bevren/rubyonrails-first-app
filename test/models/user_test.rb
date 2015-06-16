040e118b4dc57b7b0b2dc52f47cebc4cb6aaba3a
duxducis <9inedev@gmail.com>
2015-06-16 13:25:09 +0000

    User model and tests

---
+++ test/models/user_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+ 
+ def setup
+   
+   @user = User.new(name: "Batuhan", email:"example@asd.com")
+   
+ end
+ 
+ test "should be valid" do
+   assert @user.valid?
+ end
+ 
+ test "name should be present" do
+   @user.name = "     "
+   assert_not @user.valid?
+ end
+ 
+ test "email should be present" do
+   @user.email = "     "
+   assert_not @user.valid?
+ end
+ 
+ test "name should be not too long" do
+   @user.name= "a" * 51
+   assert_not @user.valid?
+ end
+ 
+ test "name should be not too short" do
+   @user.name= "a" * 3
+   assert_not @user.valid?
+ end
+ 
+ test "email should be valid address" do
+   valid_adresses = %w[naber@lo.com de_hayde@lo.com hadi_123@ulan.com]
+   
+   valid_adresses.each do |valid_adress|
+     @user.email = valid_adress
+     assert @user.valid?, "#{valid_adress} should be valid"
+   end
+   
+ end
+ 
+ test "email should be invalid address" do
+   invalid_adresses = %w[@l?.? naber naber@man..com naber@ula+sie.com
+                         naber@naber.+com]
+    invalid_adresses.each do |invalid_adresss|
+      @user.email = invalid_adresss
+      assert_not @user.valid?, "#{invalid_adresss} should be invalid"
+    end
+ end
+ 
+ 
+end
